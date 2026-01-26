d97640a77d03a9f76ff6fcfddf4db360a7171e60
HimAYouN <himayoun7931@gmail.com>
2026-01-26 10:29:02 +0530

    Purana code (incomplete)

--- client/src/pages/student/courses/WatchCourse.jsx
+++ client/src/pages/student/courses/WatchCourse.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAuth } from '../../../context/AuthContext';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 function WatchCourse() {
     const { user } = useAuth();
+    const { courseId } = useParams;
+    const token = localStorage.getItem("token");
 
-    return <div>
-        {user}
-    </div>;
+    useEffect(() => {
+        const fetchCourse = async () => {
+            try {
+                const response = await axios.get(
+                    `localhost:3000/api/v1/courses/${token}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+            } catch (error) {
+                console.log(
+                    'ERR SOMETHIHNG WENT WRONG IN /pages/student/WatchCourse.jsx',
+                    error
+                );
+            }
+        };
+
+        fetchCourse();
+    }, []);
+
+    return <div>{JSON.stringify(user)}</div>;
 }
 
 export default WatchCourse;
